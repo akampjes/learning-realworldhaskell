5208eb6be5fe837f832a6e4521900e34bcf9596b
Andrew Kampjes <a.kampjes@gmail.com>
2015-11-21 23:28:54 +1300

    where

--- ch03/Lending.hs
+++ ch03/Lending.hs
@@ -3,3 +3,9 @@ lend amount balance = let reserve    = 100
                       in if balance < reserve
                          then Nothing
                          else Just newBalance
+
+lend2 amount balance if amount < reserve
+                     then Just newbalance
+                     else Nothing
+    where reserve    = 100
+          newbalance = balance - amount
